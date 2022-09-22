5d28fa8fa43ab2e0f9900420527bb7f147b1ba3b
britathompson <74376336+britathompson@users.noreply.github.com>
2022-09-22 11:05:12 -0500

    added useEffect to WeatherForecast component to update forecast on reload

--- src/WeatherForecast.js
+++ src/WeatherForecast.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect, useLayoutEffect, useSyncExternalStore } from "react";
+import React, { useState, useEffect } from "react";
 import WeatherForecastDay from "./WeatherForecastDay";
 import "./WeatherForecast.css";
 import axios from "axios";
@@ -7,7 +7,9 @@ export default function WeatherForecast(props) {
   let [loaded, setLoaded] = useState(false);
   let [forecastData, setForecastData] = useState(null);
 
-   
+   useEffect(() => {
+    setLoaded(false);
+   }, [props.coordinates]);
 
   function handleResponse(response) {
     setForecastData(response.data.daily);
